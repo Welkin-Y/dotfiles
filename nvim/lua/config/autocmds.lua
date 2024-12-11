dd47bab5aeaa5cdc9ad5bd0bbad4be3fdba3b16f
Welkin <zywelkin@gmail.com>
2024-12-12 00:53:40 +0900

    Add config
    add lazyvim config for nvim

---
+++ nvim/lua/config/autocmds.lua
@@ -0,0 +1,18 @@
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+-- Add any additional autocmds here
+
+local clip_path = "/mnt/c/Windows/System32/clip.exe" -- Change this path as per your mount point
+
+if vim.fn.executable(clip_path) == 1 then
+  vim.api.nvim_create_augroup("WSLYank", { clear = true })
+  vim.api.nvim_create_autocmd("TextYankPost", {
+    group = "WSLYank",
+    pattern = "*",
+    callback = function()
+      if vim.v.event.operator == "y" then
+        vim.fn.system(clip_path, vim.fn.getreg('"')) -- Use the default register
+      end
+    end,
+  })
+end
